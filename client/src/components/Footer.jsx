94538a36d83fb8463099ef2b935317f353f5fb63
Joseph Maina <jmaina@kippra.or.ke>
2023-02-10 12:55:36 +0300

    buttom logo cursor pointer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => (
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
     <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
       <div className="flex flex-[0.5] justify-center items-center">
-        <img src={logo} alt="logo" className="w-32" />
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
       <div className="flex flex-1 justify-evenly items-center flex-wrap sm:mt-0 mt-5 w-full">
         <p className="text-white text-base text-center mx-2 cursor-pointer hover:text-[#1de1fd]">Market</p>
@@ -18,7 +18,7 @@ const Footer = () => (
     </div>
 
     <div className="flex justify-center items-center flex-col mt-5">
-      <p className="text-white text-sm text-center">Come join us and hear for the unexpected miracle</p>
+      <p className="text-white text-sm text-center">Buy, trade, and hold cryptocurrencies on Krypt</p>
       <p className="text-white text-sm text-center font-medium mt-2">Krypt.com</p>
     </div>
 
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -64,3 +64,5 @@ const Signup = () => {
 };
 
 export default Signup;
+
+
