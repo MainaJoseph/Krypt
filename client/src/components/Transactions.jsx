6e16375b6f5dc36677bd53f4621ed240bd57f563
Joseph Maina <jmaina@kippra.or.ke>
2023-02-13 10:38:33 +0300

    transaction-card-bg-color

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -11,13 +11,13 @@ const TransactionsCard = ({ addressTo, addressFrom, timestamp, message, keyword,
   const gifUrl = useFetch({ keyword });
 
   return (
-    <div className="bg-[#181918] m-4 flex flex-1
+    <div className="bg-[#00020f] m-4 flex flex-1
       2xl:min-w-[450px]
       2xl:max-w-[500px]
       sm:min-w-[270px]
       sm:max-w-[300px]
       min-w-full
-      flex-col p-3 rounded-md hover:shadow-2xl"
+      flex-col p-3 rounded-lg hover:shadow-2xl"
     >
       <div className="flex flex-col items-center w-full mt-3">
         <div className="display-flex justify-start w-full mb-6 p-2">
