c4c32a505717ad37e3ae0ccaf1a8050b1d907fdc
Joseph Maina <jmaina@kippra.or.ke>
2023-02-14 13:03:05 +0300

    Reverse_try fix3

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,7 +17,14 @@ const Navbar = () => {
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-      
+      <div class="flex">
+  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Market">Market</div>
+  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Trade">Trade</div>
+  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Exchange">Exchange</div>
+  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Tutorials">Tutorials</div>
+  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Wallet">Wallet</div>
+  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Transactions"><a href="https://ropsten.etherscan.io/address/0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE" target="_blank" rel="noreferrer" title="Transactions">Transactions</a></div>
+</div>
 
         <li
          className="transition ease-in-out delay-150 bg-[#2952e3] py-2 px-7 mx-4 rounded-md cursor-pointer hover:-translate-y-1 hover:scale-110 hover:bg-[#1de1fd] font-semibold">
