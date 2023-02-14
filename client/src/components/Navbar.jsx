4ca0342fc6936a1acc5cd3463e608fb94ac9a744
Joseph Maina <jmaina@kippra.or.ke>
2023-02-14 13:53:02 +0300

    _try fix4

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -17,24 +17,12 @@ const Navbar = () => {
         <img src={logo} alt="logo" className="w-32 cursor-pointer" />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-      <div class="flex">
-  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Market">Market</div>
-  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Trade">Trade</div>
-  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Exchange">Exchange</div>
-  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Tutorials">Tutorials</div>
-  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Wallet">Wallet</div>
-  <div class="px-3 py-2 text-white cursor-pointer hover:text-[#1de1fd]" title="Transactions"><a href="https://ropsten.etherscan.io/address/0x8aa395Ab97837576aF9cd6946C79024ef1acfdbE" target="_blank" rel="noreferrer" title="Transactions">Transactions</a></div>
-</div>
-
-        <li
-         className="transition ease-in-out delay-150 bg-[#2952e3] py-2 px-7 mx-4 rounded-md cursor-pointer hover:-translate-y-1 hover:scale-110 hover:bg-[#1de1fd] font-semibold">
-          Register
+        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+          <NavBarItem key={item + index} title={item} />
+        ))}
+        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+          Login
         </li>
-
-        <li className="transition ease-in-out delay-150 bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:-translate-y-1 hover:scale-110 hover:bg-[#1de1fd] duration-300 font-semibold">
-        Login
-      </li>
-
       </ul>
       <div className="flex relative">
         {!toggleMenu && (
@@ -49,19 +37,9 @@ const Navbar = () => {
             flex flex-col justify-start items-end rounded-md blue-glassmorphism text-white animate-slide-in"
           >
             <li className="text-xl w-full my-2"><AiOutlineClose onClick={() => setToggleMenu(false)} /></li>
-            {["Market","Trade", "Exchange", "Tutorials", "Wallet", "Transactions"].map(
+            {["Market", "Exchange", "Tutorials", "Wallets"].map(
               (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
             )}
-
-<ul className="h-56 grid grid-cols-2 gap-15 content-start w-[70vw] h-screen shadow-2xl md:hidde list-none">
-              <li className="transition ease-in-out delay-150 bg-[#2952e3] py-2 px-7 mx-4 rounded-md cursor-pointer hover:-translate-y-1 hover:scale-110 hover:bg-[#1de1fd] font-semibold">
-          Register
-        </li>
-        <li className="transition ease-in-out delay-150 bg-[#2952e3] py-2 px-7 mx-4 rounded-md cursor-pointer hover:-translate-y-1 hover:scale-110 hover:bg-[#1de1fd] font-semibold">
-          Login
-        </li>
-           </ul>
-           
           </ul>
         )}
       </div>
