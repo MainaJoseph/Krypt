5020cc186fac96bec1c7540b34ea1d6ae841c813
Joseph Maina <jmaina@kippra.or.ke>
2023-02-14 13:57:47 +0300

    remove exports

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,7 +1,5 @@
 export { default as Loader } from "./Loader";
 export { default as Navbar } from "./Navbar";
-export { default as Signup_navbar } from './Signup_navbar';
-export { default as Signup} from './Signup';
 export { default as Welcome } from "./Welcome";
 export { default as Footer } from "./Footer";
 export { default as Services } from "./Services";
